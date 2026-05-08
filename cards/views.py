79c61f62fb4758714a0e4dfa0442eaba0658a178
Miguel <nunezmiguel.tj@gmail.com>
2026-05-08 16:15:56 -0700

    filter added (grade, company)

--- cards/views.py
+++ cards/views.py
@@ -15,7 +15,13 @@ def serialize_card(card):
 def card_list(request):
     if request.method != 'GET':
         return JsonResponse({'error': 'Method not allowed'}, status=405)
-    cards = Card.objects.all()
+    grade = request.GET.get('grade')
+    company = request.GET.get('company')
+    cards = Card.objects.select_related('company').all()
+    if grade:
+        cards = cards.filter(grade=grade)
+    if company:
+        cards = cards.filter(company__name=company.upper())
     data = []
     for card in cards:
         data.append(serialize_card(card))
